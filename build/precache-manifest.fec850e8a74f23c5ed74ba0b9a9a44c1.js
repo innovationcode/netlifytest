1e2e13cda7658586b9129fad4a4af1e420600c72
lajawanti <myemail>
2019-03-12 23:35:23 -0400

    sfd

---
+++ build/precache-manifest.fec850e8a74f23c5ed74ba0b9a9a44c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4f791ee296202f776797",
+    "url": "/static/js/main.9a1be35a.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "4f791ee296202f776797",
+    "url": "/static/css/main.8182958d.chunk.css"
+  },
+  {
+    "revision": "8c5a8f84d258b917548335575ffc96c9",
+    "url": "/index.html"
+  }
+];
